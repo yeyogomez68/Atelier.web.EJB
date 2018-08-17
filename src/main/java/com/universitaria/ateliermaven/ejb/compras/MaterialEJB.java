6079fba71f0fec60851db4ce4341c6f84ce24f3b
Jeisson Gomez <Jeisson Gomez@DESKTOP-FV6VQJ9>
2018-08-17 17:05:58 -0500

    menu dinamico

---
+++ src/main/java/com/universitaria/ateliermaven/ejb/compras/MaterialEJB.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.universitaria.ateliermaven.ejb.compras;
+
+import com.universitaria.atelier.web.jpa.AbstractFacade;
+import com.universitaria.atelier.web.jpa.Marca;
+import com.universitaria.atelier.web.jpa.Material;
+import com.universitaria.atelier.web.jpa.Materialtipo;
+import com.universitaria.atelier.web.utils.MaterialUtil;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.faces.model.SelectItem;
+
+/**
+ *
+ * @author jeisson.gomez
+ */
+@Stateless
+public class MaterialEJB extends AbstractFacade<Material>{
+
+    public MaterialEJB() {
+        super(Material.class);
+    }   
+    
+    public List<Material> getMateriales(){
+        try {
+            return (ArrayList<Material>) em.createNamedQuery("Material.findAll",Material.class).getResultList();
+        } catch (NullPointerException e){
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }       
+        return null;
+    }
+    
+        public List<SelectItem> getSelectItemMaterial(){
+        List<SelectItem> lista = new ArrayList<>();
+        try {
+            for(Material mate :(ArrayList<Material>) em.createNamedQuery("Material.findAll",Material.class).getResultList()){
+                lista.add(new SelectItem(mate.getMaterialId(),mate.getMaterialNombre()));
+            }
+            return lista;
+        } catch (NullPointerException e){
+            return lista;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }            
+        return lista;
+    }
+    
+    public boolean setCrearMaterial(MaterialUtil material){
+        try {
+            Material mate = new Material();  
+            mate.setMaterialNombre(material.getNombre());
+            mate.setMaterialReference(material.getReferencia());
+            mate.setMaterialTipoId(em.find(Materialtipo.class, Integer.valueOf(material.getTipoId())));
+            mate.setMarcaId(em.find(Marca.class, Integer.valueOf(material.getMarcaId())));
+            create(mate);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    
+    public boolean setModificarMaterial(MaterialUtil material){
+        try {
+            Material mate = new Material();
+            mate = em.find(Material.class, Integer.parseInt(material.getMaterialId()));
+            mate.setMaterialNombre(material.getNombre());
+            mate.setMaterialReference(material.getReferencia());
+            mate.setMaterialTipoId(em.find(Materialtipo.class, Integer.valueOf(material.getTipoId())));
+            mate.setMarcaId(em.find(Marca.class, Integer.valueOf(material.getMarcaId())));
+            edit(mate);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
