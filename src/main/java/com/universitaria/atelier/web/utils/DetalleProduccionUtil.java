acb4b8d107941d98110ddd33165aaf97feb88271
Jeisson Gomez <Jeisson Gomez@DESKTOP-FV6VQJ9>
2018-08-25 00:20:00 -0500

    Merge origin/master
    Conflicts:
    	src/main/java/com/universitaria/ateliermaven/web/compras/RequerimientoManagedBean.java
    	src/main/webapp/WEB-INF/faces-config.xml
    	src/main/webapp/resources/css/styles.css
    	src/main/webapp/resources/jsf/compras/requerimiento.xhtml

---
+++ src/main/java/com/universitaria/atelier/web/utils/DetalleProduccionUtil.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.universitaria.atelier.web.utils;
+
+import java.util.Date;
+
+/**
+ *
+ * @author SoulHunter
+ */
+public class DetalleProduccionUtil {
+
+    private String produccionDetaId;
+    private String materialId;
+    private String produccionDetaCant;
+    private Date produccionDetaFecha;
+    private String produccionId;
+
+    public String getProduccionDetaId() {
+        return produccionDetaId;
+    }
+
+    public void setProduccionDetaId(String produccionDetaId) {
+        this.produccionDetaId = produccionDetaId;
+    }
+
+    public String getMaterialId() {
+        return materialId;
+    }
+
+    public void setMaterialId(String materialId) {
+        this.materialId = materialId;
+    }
+
+    public String getProduccionDetaCant() {
+        return produccionDetaCant;
+    }
+
+    public void setProduccionDetaCant(String produccionDetaCant) {
+        this.produccionDetaCant = produccionDetaCant;
+    }
+
+    public Date getProduccionDetaFecha() {
+        return produccionDetaFecha;
+    }
+
+    public void setProduccionDetaFecha(Date produccionDetaFecha) {
+        this.produccionDetaFecha = produccionDetaFecha;
+    }
+
+    public String getProduccionId() {
+        return produccionId;
+    }
+
+    public void setProduccionId(String produccionId) {
+        this.produccionId = produccionId;
+    }
+
+    
+}
